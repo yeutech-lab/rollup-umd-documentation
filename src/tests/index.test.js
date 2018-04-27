7ea411c5758a3af2e5b39d0e985f8a57345f884f
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2018-04-27 21:28:19 +0700

    rollup-doc

--- src/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const exported = require('../index');
-
-describe('exported', () => {
-  Object.keys(exported).forEach((key) => {
-    it(`should export ${key}`, () => {
-      expect(exported[key]).toBeDefined();
-    });
-  });
-});
