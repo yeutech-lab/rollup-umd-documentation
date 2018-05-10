c87b8a309c3aa9121a8d48f28ff0b95ea1b499d4
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-10 10:51:12 +0700

    Added pre-loader html/css to styleguide.config.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 
 ## [?NEXT?] - ????-??-??
 
-- TODO: write changelog before preparing next tag
+- Added loader.
 
 ## [v0.0.12] - 2018-05-07
 
--- src/styleguide.config.js
+++ src/styleguide.config.js
@@ -58,7 +58,82 @@ export const config = {
       ${generateCSSReferences(css, publicPath)}
     </head>
     <body>
-    <div id="rsg-root"></div>
+    <div id="rsg-root">
+      <style>
+        .loader {
+          position: absolute;
+          top: 50%;
+          left: 48%;
+        }
+
+        .line {
+          animation: expand 1.5s ease-in-out infinite;
+          border-radius: 10px;
+          display: inline-block;
+          transform-origin: center center;
+          margin: 0 3px;
+          width: 1px;
+          height: 25px;
+        }
+
+        .line:nth-child(1) {
+          background: #22d4e8;
+        }
+
+        .line:nth-child(2) {
+          animation-delay: 180ms;
+          background: #4922e8;
+        }
+
+        .line:nth-child(3) {
+          animation-delay: 360ms;
+          background: #bd22e8;
+        }
+
+        .line:nth-child(4) {
+          animation-delay: 540ms;
+          background: #e8226a;
+        }
+
+        .line:nth-child(5) {
+          animation-delay: 540ms;
+          background: #e86a22;
+        }
+
+        .line:nth-child(6) {
+          animation-delay: 360ms;
+          background: #e8de22;
+        }
+
+        .line:nth-child(7) {
+          animation-delay: 180ms;
+          background: #8ee822;
+        }
+
+        .line:nth-child(8) {
+          background: #22e853;
+        }
+
+        @keyframes expand {
+          0% {
+            transform: scale(1);
+          }
+          25% {
+            transform: scale(2);
+          }
+        }
+      </style>
+      <div class="loader">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+    </div>
     ${generateJSReferences(js, publicPath)}
     </body>
   </html>`,
