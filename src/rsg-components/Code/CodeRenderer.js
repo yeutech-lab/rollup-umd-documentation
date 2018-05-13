3e18121af483fa0908b843f6769338ad9168625f
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-13 23:43:25 +0700

    Update Code/CodeRenderer.js and Type/TypeRenderer.js, bs component added and styleguidist styles removed

--- src/rsg-components/Code/CodeRenderer.js
+++ src/rsg-components/Code/CodeRenderer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Code from 'bootstrap-styled/lib/Code';
 
 export function CodeRenderer({ className, children }) {
-	const isHighlighted = className && className.indexOf('lang-') !== -1;
-	if (isHighlighted) {
-		return <Code className={className} dangerouslySetInnerHTML={{ __html: children }} />;
-	}
-	return <Code className={className}>{children}</Code>;
+  const isHighlighted = className && className.indexOf('lang-') !== -1;
+  if (isHighlighted) {
+    return <Code className={className} dangerouslySetInnerHTML={{ __html: children }} />;
+  }
+  return <Code className={className}>{children}</Code>;
 }
 
 export default CodeRenderer;
--- src/rsg-components/Type/TypeRenderer.js
+++ src/rsg-components/Type/TypeRenderer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Code from 'rsg-components/Code';
 
 export function TypeRenderer({ children }) {
-	return (
-      <Code>{children}</Code>
-	);
+  return (
+    <Code>{children}</Code>
+  );
 }
 
 export default TypeRenderer;
