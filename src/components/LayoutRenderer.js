9b62125ac7a6661facf8f9a468d26afaf086860e
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-15 15:06:00 +0700

    Update LayoutRenderer component with rsg style removed and basic menu added.

--- src/components/LayoutRenderer.js
+++ src/components/LayoutRenderer.js
@@ -1,66 +1,23 @@
 /* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Styled from 'react-styleguidist/lib/rsg-components/Styled';
 import Logo from 'react-styleguidist/lib/rsg-components/Logo';
-
-import cn from 'classnames';
+import Ribbon from 'react-styleguidist/lib/rsg-components/Ribbon';
 import theme from 'bootstrap-styled/lib/theme';
+import NavigationBar from 'navigation-bar';
+import { theme as navTheme } from 'navigation-bar';
+import PageWrapper from 'navigation-bar/lib/NavigationBar';
+
 import BootstrapProvider from 'bootstrap-styled/lib/BootstrapProvider';
+import A from 'bootstrap-styled/lib/A';
+import Img from 'bootstrap-styled/lib/Img';
+import Nav from 'bootstrap-styled/lib/Nav';
+import ListGroup from 'bootstrap-styled/lib/ListGroup';
+import Button from 'bootstrap-styled/lib/Button';
+import NavLink from 'bootstrap-styled/lib/Nav/NavLink';
 
 import defaultLogo from './logo';
 
-const styles = ({
-  color, fontFamily, fontSize, sidebarWidth, mq, space, maxWidth,
-}) => ({
-  root: {
-    backgroundColor: color.baseBackground,
-  },
-  hasSidebar: {
-    paddingLeft: sidebarWidth,
-    [mq.small]: {
-      paddingLeft: 0,
-    },
-  },
-  content: {
-    maxWidth,
-    padding: [[space[2], space[4]]],
-    margin: [[0, 'auto']],
-    [mq.small]: {
-      padding: space[2],
-    },
-    display: 'block',
-  },
-  sidebar: {
-    backgroundColor: color.sidebarBackground,
-    border: [[color.border, 'solid']],
-    borderWidth: [[0, 1, 0, 0]],
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    width: sidebarWidth,
-    overflow: 'auto',
-    '-webkit-overflow-scrolling': 'touch',
-    [mq.small]: {
-      position: 'static',
-      width: 'auto',
-      borderWidth: [[1, 0, 0, 0]],
-      paddingBottom: space[0],
-    },
-  },
-  logo: {
-    padding: space[2],
-    borderBottom: [[1, color.border, 'solid']],
-  },
-  footer: {
-    display: 'block',
-    color: color.light,
-    fontFamily: fontFamily.base,
-    fontSize: fontSize.small,
-    textAlign: 'right',
-  },
-});
 
 /**
  * This is the main layout for the whole documentation.
@@ -71,12 +28,16 @@ const styles = ({
 function LayoutRenderer({
    theme, classes, className, title, children, toc, hasSidebar, logo, bottomLogo, bottomLogoText, logoHref, bottomLogoHref
  }) {
+  const layoutTheme = {
+    ...theme,
+    ...navTheme,
+  }
   return (
-    <BootstrapProvider theme={theme}>
-      <div className={cn(className, classes.root, hasSidebar && classes.hasSidebar)}>
-        <main className={classes.content}>
+    <BootstrapProvider theme={layoutTheme}>
+      <div>
+        <main style={{ marginLeft: '240px'}}>
           {children}
-          <footer className={classes.footer}>
+          <footer>
             <span>{bottomLogoText}</span>
             <a
               href={bottomLogoHref}
@@ -94,36 +55,49 @@ function LayoutRenderer({
           </footer>
         </main>
         {hasSidebar && (
-          <div className={classes.sidebar}>
-            <div className={classes.logo}>
-              {logoHref ? (
-                <a
-                  href={logoHref}
-                  target="_blank"
-                  alt="Yeutech Company Limited"
-                  title="Yeutech Company Limited"
-                >
-                  <img
-                    src={`data:image/png;base64,${logo}`}
-                    height="70px"
-                    alt={logo === defaultLogo ? 'Yeutech Company Limited logo' : 'logo'}
-                    title={logo === defaultLogo ? 'Yeutech Company Limited' : 'Brand logo'}
-                  />
-                </a>
-              ) : (
+          <NavigationBar
+            fixed="top"
+            className="flex-lg-column flex-row"
+            button={{
+              component: Button,
+              className: 'd-lg-none mr-lg-5 mr-3 my-auto btn-primary',
+            }}
+            offsetNav={{
+              right: false,
+              show: 'lg',
+              bgColor: 'inverse',
+              top: '70',
+            }}
+          >
+            {logoHref ? (
+              <a
+                href={logoHref}
+                target="_blank"
+                alt="Yeutech Company Limited"
+                title="Yeutech Company Limited"
+              >
                 <img
                   src={`data:image/png;base64,${logo}`}
                   height="70px"
                   alt={logo === defaultLogo ? 'Yeutech Company Limited logo' : 'logo'}
                   title={logo === defaultLogo ? 'Yeutech Company Limited' : 'Brand logo'}
                 />
-              )}
-
-              <Logo>{title}</Logo>
-            </div>
-            {toc}
-          </div>
+              </a>
+            ) : (
+              <img
+                src={`data:image/png;base64,${logo}`}
+                height="70px"
+                alt={logo === defaultLogo ? 'Yeutech Company Limited logo' : 'logo'}
+                title={logo === defaultLogo ? 'Yeutech Company Limited' : 'Brand logo'}
+              />
+            )}
+            <Logo color="white">{title}</Logo>
+            <ListGroup>
+              {toc}
+            </ListGroup>
+          </NavigationBar>
         )}
+        <Ribbon />
       </div>
     </BootstrapProvider>
   );
@@ -140,8 +114,6 @@ LayoutRenderer.defaultProps = {
 };
 
 LayoutRenderer.propTypes = {
-  /** @ignore */
-  classes: PropTypes.object.isRequired,
   /** @ignore */
   className: PropTypes.string,
   /** @ignore */
@@ -166,4 +138,4 @@ LayoutRenderer.propTypes = {
   bottomLogoText: PropTypes.string,
 };
 
-export default Styled(styles)(LayoutRenderer);
+export default LayoutRenderer;
