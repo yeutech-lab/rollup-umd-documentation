031e27d7fc2d1709165a5ecc328472478e0964db
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-18 19:47:57 +0700

    Add newcomponent Main in components/ and imported in LayoutRenderer

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+// import bp from 'bootstrap-styled-mixins/lib/breakpoints';
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  margin-left: 300px;
+`;
+
+export default Main;
