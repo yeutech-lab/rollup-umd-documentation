8a1499d3725b18ae8fddc7d78cc15dba46255c81
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-23 22:08:41 +0700

    fix(menu): Add collapsed link to menu
    Add collapsed link if they have sub link

--- src/rsg-components/ComponentsList/ComponentsListRenderer.js
+++ src/rsg-components/ComponentsList/ComponentsListRenderer.js
@@ -1,37 +1,76 @@
-/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Ul from 'bootstrap-styled/lib/Ul';
-import Li from 'bootstrap-styled/lib/Li';
+import Collapse from 'bootstrap-styled/lib/Collapse';
 import Link from '../Link';
 
-export function ComponentsListRenderer({ items }) {
-  items = items.filter(item => item.name);
-
-  if (!items.length) {
-    return null;
-  }
-
-  return (
-    <Ul>
-      {items.map(({ heading, name, href, content }) => (
-        <Li
-          key={href}
-        >
-          <Link href={href}>
-            {name}
-          </Link>
-          {content}
-        </Li>
-      ))}
-    </Ul>
-  );
-}
-
-ComponentsListRenderer.propTypes = {
-  items: PropTypes.array.isRequired,
-  useIsolatedLinks: PropTypes.bool,
+export const defaultProps = {
+  items: [], // eslint-disable-line react/default-props-match-prop-types
 };
 
+export const propTypes = {
+  /**
+   * @ignore
+   */
+  items: PropTypes.array.isRequired,
+  useIsolatedLinks: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types, react/require-default-props
+};
+
+class ComponentsListRenderer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static defaultProps = defaultProps;
+
+  /* eslint-disable react/no-unused-prop-types */
+  static propTypes = propTypes;
+  /* eslint-enable react/no-unused-prop-types */
+
+  state = {
+    itemList: this.props.items.filter((item) => item.name),
+  }
+
+  componentWillMount() {
+    const newState = {};
+    this.state.itemList.map(({ heading, name }) => {
+      if (heading) {
+        newState[`${name}-is-open`] = false;
+        this.setState({ ...newState });
+      }
+      return null; // todo: I want to know how to do when we don't have specific return?
+    });
+  }
+  onClick = (name) => {
+    this.setState({
+      [`${name}-is-open`]: !this.state[`${name}-is-open`],
+    });
+  };
+
+  render() {
+    const {
+      itemList,
+    } = this.state;
+    if (!itemList.length) {
+      return null;
+    }
+    return (
+      <ul>
+        {itemList.map(({
+          heading,
+          name,
+          href,
+          content,
+        }) => (
+          <li
+            key={href}
+          >
+            <Link href={href} onClick={() => heading && this.onClick(name)}>
+              {name}
+            </Link>
+            <Collapse isOpen={this.state[`${name}-is-open`]}>
+              {content}
+            </Collapse>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
 export default ComponentsListRenderer;
-/* eslint-disable */
