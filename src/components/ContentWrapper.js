281be720c8952c40d199050b38042114ed5e770f
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-25 14:03:35 +0700

    fix(components): Update ContentWrapper.js with new style padding and LayoutRenderer.js with Ribbon c

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -3,13 +3,13 @@ import bp from 'bootstrap-styled-mixins/lib/breakpoints';
 
 const ContentWrapper = styled.div`
   ${(props) => `
-    padding: 0 20px;
+    padding: 0 20px 0 20px;
     ${bp.down(
     'md',
     props.theme['$grid-breakpoints'],
     `
-        padding: 0 10px;
-      `
+      padding: 0 10px 0 10px;
+    `
   )};
   `}
 `;
