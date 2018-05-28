a2432bb5de8cc622187289f6d11e442ad65ca741
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-05-28 20:58:06 +0700

    test(ComponentsListRenderer): Update test for ComponentsListRenderer covered to 100%

--- src/rsg-components/ComponentsList/tests/ComponentsListRenderer.test.js
+++ src/rsg-components/ComponentsList/tests/ComponentsListRenderer.test.js
@@ -2,10 +2,7 @@
  * Testing our link component
  */
 import React from 'react';
-import {
-  // shallow,
-  mount,
-} from 'enzyme';
+import { mount } from 'enzyme';
 import BootstrapProvider from 'bootstrap-styled/lib/BootstrapProvider';
 import '!!style-loader!css-loader!../../../node_modules/font-awesome/css/font-awesome.css'; // eslint-disable-line import/no-webpack-loader-syntax
 import ComponentsListRenderer, { defaultProps } from '../ComponentsListRenderer';
@@ -13,13 +10,23 @@ import ComponentsListRenderer, { defaultProps } from '../ComponentsListRenderer'
 describe('<ComponentsListRenderer />', () => {
   const { theme } = defaultProps;
   let props;
-  let mappedItems;
-  // let onClick = jest.fn();
 
   beforeEach(() => {
     props = Object.assign(defaultProps, {
-      items: [{ href: 'blank#!/Button' }],
-      useIsolatedLinks: false,
+      items: [
+        {
+          name: 'First component',
+          href: '/#first-component',
+          filepath: 'first-component.md',
+          heading: false,
+          level: 0,
+          content: false,
+          collapse: true,
+          slug: 'first-component',
+          components: [],
+          sections: [],
+        },
+      ],
     });
   });
 
@@ -27,7 +34,6 @@ describe('<ComponentsListRenderer />', () => {
     const renderedComponent = mount(
       <BootstrapProvider theme={theme} injectGlobal={false}>
         <ComponentsListRenderer
-          items={mappedItems}
           {...props}
         />
       </BootstrapProvider>
@@ -38,25 +44,161 @@ describe('<ComponentsListRenderer />', () => {
     const renderedComponent = mount(
       <BootstrapProvider theme={theme} injectGlobal={false}>
         <ComponentsListRenderer
-          items={mappedItems}
           {...props}
         />
       </BootstrapProvider>
     );
-    expect(renderedComponent.find('ComponentsListRendererUnstyled').props().items).toEqual([{ href: 'blank#!/Button' }]);
+    expect(renderedComponent.find('ComponentsListRendererUnstyled').props().items).toEqual([{
+      collapse: true,
+      components: [],
+      content: false,
+      filepath: 'first-component.md',
+      heading: false,
+      href: '/#first-component',
+      level: 0,
+      name: 'First component',
+      sections: [],
+      slug: 'first-component',
+    }]);
   });
-  // it('should render an ComponentsListRenderer with an onClick that triggers collapse', () => {
-  //   const renderedComponent = mount(
-  //     <BootstrapProvider theme={theme} injectGlobal={false}>
-  //       <ComponentsListRenderer
-  //         items={mappedItems}
-  //         {...props}
-  //       />
-  //     </BootstrapProvider>
-  //   );
-  //   const divButton = renderedComponent.find('Styled(ComponentsListRendererUnstyled)');
-  //   expect(renderedComponent.find('Collapse').props().isOpen).toBe(false);
-  //   divButton.simulate('click');
-  //   expect(renderedComponent.find('Collapse').props().isOpen).toBe(true);
-  // });
+  it('should render an ComponentsListRenderer without collapse', () => {
+    const newProps = {
+      theme,
+      items: [
+        {
+          name: 'First component',
+          href: '/#first-component',
+          filepath: 'first-component.md',
+          heading: true,
+          level: 0,
+          content: false,
+          collapse: false,
+          slug: 'first-component',
+          components: [],
+          sections: [
+            {
+              name: 'Sub component',
+              href: '/#sub-component',
+              filepath: 'sub-component.md',
+              heading: false,
+              level: 0,
+              content: false,
+              collapse: false,
+              slug: 'sub-component',
+              components: [],
+              sections: [],
+            },
+          ],
+        },
+      ],
+    };
+    const renderedComponent = mount(
+      <BootstrapProvider theme={theme} injectGlobal={false}>
+        <ComponentsListRenderer
+          {...newProps}
+        />
+      </BootstrapProvider>
+    );
+    expect(renderedComponent.contains('Collapse')).toBe(false);
+  });
+  it('should render an ComponentsListRenderer with an onClick event that triggers collapse', () => {
+    const newProps = {
+      theme,
+      items: [
+        {
+          name: 'First component',
+          href: '/#first-component',
+          filepath: 'first-component.md',
+          heading: true,
+          level: 0,
+          content: false,
+          collapse: true,
+          slug: 'first-component',
+          components: [],
+          sections: [
+            {
+              name: 'Sub component',
+              href: '/#sub-component',
+              filepath: 'sub-component.md',
+              heading: false,
+              level: 0,
+              content: false,
+              collapse: true,
+              slug: 'sub-component',
+              components: [],
+              sections: [],
+            },
+          ],
+        },
+      ],
+    };
+    const renderedComponent = mount(
+      <BootstrapProvider theme={theme} injectGlobal={false}>
+        <ComponentsListRenderer
+          {...newProps}
+        />
+      </BootstrapProvider>
+    );
+    const divButton = renderedComponent.find('div.list-button');
+    expect(renderedComponent.find('Collapse').props().isOpen).toBe(false);
+    divButton.simulate('click');
+    expect(renderedComponent.find('Collapse').props().isOpen).toBe(true);
+  });
+  it('should render an ComponentsListRenderer with an onKeyPress event that triggers collapse', () => {
+    const newProps = {
+      theme,
+      items: [
+        {
+          name: 'First component',
+          href: '/#first-component',
+          filepath: 'first-component.md',
+          heading: true,
+          level: 0,
+          content: false,
+          collapse: true,
+          slug: 'first-component',
+          components: [],
+          sections: [
+            {
+              name: 'Sub component',
+              href: '/#sub-component',
+              filepath: 'sub-component.md',
+              heading: false,
+              level: 0,
+              content: false,
+              collapse: true,
+              slug: 'sub-component',
+              components: [],
+              sections: [],
+            },
+          ],
+        },
+      ],
+    };
+    const renderedComponent = mount(
+      <BootstrapProvider theme={theme} injectGlobal={false}>
+        <ComponentsListRenderer
+          {...newProps}
+        />
+      </BootstrapProvider>
+    );
+    const divButton = renderedComponent.find('div.list-button');
+    expect(renderedComponent.find('Collapse').props().isOpen).toBe(false);
+    divButton.simulate('keyPress', { keyCode: 40 });
+    expect(renderedComponent.find('Collapse').props().isOpen).toBe(true);
+  });
+  it('should not render any children', () => {
+    const newProps = {
+      theme,
+      items: [],
+    };
+    const renderedComponent = mount(
+      <BootstrapProvider theme={theme} injectGlobal={false}>
+        <ComponentsListRenderer
+          {...newProps}
+        />
+      </BootstrapProvider>
+    );
+    expect(renderedComponent.find('ComponentsListRendererUnstyled').props().children).toBe(undefined);
+  });
 });
