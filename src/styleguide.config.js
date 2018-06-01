856d963713d3aa37081c8b954dd76716357b2ccb
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-06-01 22:31:29 +0700

    fix(styleguide-config.json): Update template with correct viewport tag.

--- src/styleguide.config.js
+++ src/styleguide.config.js
@@ -59,6 +59,7 @@ export const config = {
   <html>
     <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>${title}</title>
       <style>
         /* http://meyerweb.com/eric/tools/css/reset/ 
