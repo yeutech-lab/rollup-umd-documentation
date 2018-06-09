9ec9a06b54640a3fa696f50f14123e60080dd18e
Adrien Gadaud <adrien.gadaud@kopaxgroup.com>
2018-06-10 04:22:04 +0700

    fix(WelcomeRenderer): FIx error du to pkg import.

--- src/rsg-bs-components/Welcome/WelcomeRenderer.js
+++ src/rsg-bs-components/Welcome/WelcomeRenderer.js
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 import mapToCssModules from 'map-to-css-modules/lib';
 import cn from 'classnames';
 import Markdown from 'rsg-components/Markdown';
-import { pkg } from '../../styleguide.config';
 
 export const defaultProps = {
   theme: {
@@ -58,7 +57,7 @@ const WelcomeRendererUnstyled = (props) => {
     >
       <Markdown
         text={`
-# Welcome to ${pkg.name}!
+# Welcome to Rollup-documentation!
 
 **We couldn’t find any components** using these patterns:
 
